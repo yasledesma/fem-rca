cc45509b3137236cdb5b8c2381b2e654566a99a6
HiddenOgre <yaslesd@gmail.com>
2022-02-12 02:15:54 -0300

    Finishing styling the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import moonIcon from "../assets/moon-icon.svg";
+import sunIcon from "../assets/sun-icon.svg";
 
 const Header = () => {
   return (
     <div className='Header'>
-
+      <header className='Header__header'>
+        <h1 className='Header__title'>Where in the world?</h1>
+        <div className='Header__theme'>
+          <button className='Header__theme__btn'><img className='Header__theme__btn__img' src={moonIcon} alt="Switch theme"></img></button>
+          <span className='Header__theme__span'>Dark Mode</span>
+        </div>
+      </header>
     </div>
   );
 }
